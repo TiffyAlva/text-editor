5bf8552715680c91dae671f971cf48f2dfa891fc
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-11-07 19:21:19 -0500

    adding cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// TODO: Implement asset caching (DONE/CORRECT?)
 registerRoute();
+({ request }) => request.destination === 'image',
+new CacheFirst({
+  cacheName: 'my-image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    }),
+  ],
+});
